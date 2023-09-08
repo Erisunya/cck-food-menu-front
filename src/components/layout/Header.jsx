d124b10c8b937b4a19c1edf865ad908610ea212b
Erisunya <ojjstuff@gmail.com>
2023-09-08 14:38:34 +0800

    Implement basic header

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,13 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <>
+      <nav className={styles.header}>
+        <h1>CCK Food Menu</h1>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
