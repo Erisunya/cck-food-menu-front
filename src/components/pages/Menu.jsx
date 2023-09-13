07782ff6b659b884d8c466db68c8ab19dbcbd52b
Erisunya <ojjstuff@gmail.com>
2023-09-13 17:40:16 +0800

    Implement Lightbox viewer for menus

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,10 +1,13 @@
 import styles from "./Menu.module.css";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import Lightbox from "react-awesome-lightbox";
+import "react-awesome-lightbox/build/style.css";
 import Card from "../layout/Card";
 
 const Menu = (props) => {
   const [menus, setMenus] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
   const { placeName, stallName } = useParams();
   const navigate = useNavigate();
 
@@ -26,6 +29,10 @@ const Menu = (props) => {
     console.log(menus.images);
   }, [menus]);
 
+  const onMenuClickHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
   // Return ErrorPage if stall param is invalid
 
   return (
@@ -33,11 +40,19 @@ const Menu = (props) => {
       <button onClick={() => navigate(-1)}>Back</button>
       {menus.halal === "Y" ? <h1 className={styles.halal}>Halal</h1> : null}
       {menus.halal === "N" ? <h1 className={styles.halal}>Not Halal</h1> : null}
+      {/* {menus.images ? <Lightbox images={menus.images}></Lightbox> : null} */}
       <div className={styles.menus}>
         {menus.images?.map((image) => {
-          return <Card key={image} image={image} link={image}></Card>;
+          return (
+            <div onClick={onMenuClickHandler}>
+              <Card key={image} image={image}></Card>
+            </div>
+          );
         })}
       </div>
+      {isOpen ? (
+        <Lightbox images={menus.images} onClose={onMenuClickHandler}></Lightbox>
+      ) : null}
     </>
   );
 };
