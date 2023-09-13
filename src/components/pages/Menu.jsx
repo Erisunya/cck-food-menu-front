eb3361324ce3b5df0051a39d9ccf8360812f9e02
Erisunya <ojjstuff@gmail.com>
2023-09-13 17:04:16 +0800

    Implement Menu page

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,9 +1,45 @@
 import styles from "./Menu.module.css";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Card from "../layout/Card";
 
-const Menu = () => {
+const Menu = (props) => {
+  const [menus, setMenus] = useState({});
+  const { placeName, stallName } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getMenus = async () => {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/places/${placeName}/${stallName}`
+      );
+      const data = await response.json();
+      console.log("getMenus");
+      setMenus(data);
+    };
+
+    getMenus();
+  }, []);
+
+  useEffect(() => {
+    console.log(menus);
+    console.log(menus.images);
+  }, [menus]);
+
   // Return ErrorPage if stall param is invalid
 
-  return <h1>Menu</h1>;
+  return (
+    <>
+      <button onClick={() => navigate(-1)}>Back</button>
+      {menus.halal === "Y" ? <h1 className={styles.halal}>Halal</h1> : null}
+      {menus.halal === "N" ? <h1 className={styles.halal}>Not Halal</h1> : null}
+      <div className={styles.menus}>
+        {menus.images?.map((image) => {
+          return <Card key={image} image={image} link={image}></Card>;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Menu;
