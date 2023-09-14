5053b0ea15be75b64e44a53d3f0f6c5a78c88f5d
Erisunya <ojjstuff@gmail.com>
2023-09-14 17:00:58 +0800

    Improve styling of back buttons in Stalls and Menu

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -32,7 +32,21 @@ const Menu = (props) => {
 
   return (
     <>
-      <button onClick={() => navigate(-1)}>Back</button>
+      <button className={styles.button} onClick={() => navigate(-1)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path fill="none" d="M0 0h24v24H0z"></path>
+          <path
+            fill="currentColor"
+            d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z"
+          ></path>
+        </svg>
+        Go back
+      </button>
       {menus.halal === "Y" ? <h1 className={styles.halal}>Halal</h1> : null}
       {menus.halal === "N" ? <h1 className={styles.halal}>Not Halal</h1> : null}
       <div className={styles.menus}>
