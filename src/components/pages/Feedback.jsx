e03f506be8661c1bfbb6b7a28cf132b8e9ec5cbc
Erisunya <ojjstuff@gmail.com>
2023-09-14 18:10:29 +0800

    Implement feedback form on Feedback component

--- src/components/pages/Feedback.jsx
+++ src/components/pages/Feedback.jsx
@@ -1,7 +1,67 @@
 import styles from "./Feedback.module.css";
+import { useForm } from "react-hook-form";
 
 const Feedback = () => {
-  return <h1>Feedback Form</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className={styles.feedback}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <p className={styles.formTitle}>Leave your feedback here!</p>
+        <div className={styles.inputContainer}>
+          <label htmlFor="name">Name</label>
+          <input
+            placeholder="Joseph Tan Ah Kow"
+            type="text"
+            id="name"
+            {...register("name", { required: true })}
+          />
+        </div>
+
+        <div className={styles.inputContainer}>
+          <label htmlFor="email">Email</label>
+          <input
+            placeholder="josepthtaw@gmail.com"
+            type="text"
+            id="email"
+            {...register("email", { pattern: /^\S+@\S+$/i })}
+          />
+        </div>
+
+        <div className={styles.inputContainer}>
+          <label htmlFor="telegram">Telegram</label>
+          <input
+            placeholder="@josepthtaw"
+            type="text"
+            id="telegram"
+            {...register("telegram")}
+          />
+        </div>
+
+        <div className={styles.inputContainer}>
+          <label htmlFor="feedback">Feedback</label>
+          <textarea
+            placeholder="Can you please update the menu from ABC Chicken Rice to show the latest price?"
+            id="feedback"
+            rows={4}
+            {...register("feedback", { required: true })}
+          />
+        </div>
+
+        <button type="submit" className={styles.submit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Feedback;
