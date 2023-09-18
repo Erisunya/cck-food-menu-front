9075de66ea76118818348df54c7afdb9f8f77ba9
Erisunya <ojjstuff@gmail.com>
2023-09-18 17:34:58 +0800

    Implement ErrorPage

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -1,7 +1,21 @@
 import styles from "./ErrorPage.module.css";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <h1>Error</h1>;
+  return (
+    <>
+      <div className={styles.errorPage}>
+        <h1>Oops! It seems like this page doesn't exist.</h1>
+        <p>
+          You may return to the main page <Link to={"/"}>here</Link>.
+        </p>
+        <p>
+          Alternatively, if you believe that you have encountered a bug, please
+          submit a feedback to us over <Link to={"/feedback"}>here</Link>.
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default ErrorPage;
